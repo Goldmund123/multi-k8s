44e08d5aa5e41996bf8af213081574134d178902
Jay <jay@Charmaines-MacBook-Air.local>
2020-04-01 21:32:34 -0700

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t new4303/multi-client:latest -t new4303/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t new4303/multi-server:latest -t new4303/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t new4303/multi-worker:latest -t new4303/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push new4303/multi-client:latest
+docker push new4303/multi-server:latest
+docker push new4303/multi-worker:latest
+
+docker push new4303/multi-client:$SHA
+docker push new4303/multi-server:$SHA
+docker push new4303/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=new4303/multi-server:$SHA
+kubectl set image deployments/client-deployment server=new4303/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=new4303/multi-worker:$SHA
